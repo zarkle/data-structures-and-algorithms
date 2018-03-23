b3ba3e4de4edd6e261e7a582c6558eb823da1f77
Beverly Pham <beverly.pham@gmail.com>
2018-03-22 20:49:27 -0700

    deleteting branch

--- challenges/largest_product/largest_product.py
+++
@@ -1,6 +0,0 @@
-def largest_product(array):
-    product = 0
-        for i in array:
-            if array[i][0] * array[i][1] > 0:
-                product = array[i][0] * array[i][1]
-    return product
