a3fe3788258a36c4732281b7d4069a3df4fc058e
Beverly Pham <beverly.pham@gmail.com>
2018-03-22 20:30:52 -0700

    not working

--- challenges/largest_product/test_name.py
+++
@@ -1,6 +0,0 @@
-import name
-
-def test_largest_product(2d_array, product):
-    assert largest_product([[1, 2], [3, 4], [5, 6], [7, 8]], 56)
-    assert largest_product([[8, 20], [5, 4], [100, 100], [8, 2], [9, 3], [2, 20]], 10000)
-    assert largest_product([[3, 8], [4, 10], [10, 50]], 500)
