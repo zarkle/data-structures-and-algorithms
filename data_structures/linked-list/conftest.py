b07f6d926a405e257e5f42d06f33b3edf87a85bd
Beverly Pham <beverly.pham@gmail.com>
2018-03-26 00:01:53 -0700

    refactor into conftest.py

---
+++ data_structures/linked-list/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from linked_list import LinkedList as LL
+
+@pytest.fixture
+def empty_ll():
+    """fixture for empty array"""
+    return LL()
+
+
+@pytest.fixture
+def small_ll():
+    """fixture for short array"""
+    return LL([1, 2, 3, 4])
