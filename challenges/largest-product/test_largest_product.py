06d240aa253f976785faf68cf2f1b3cbc7f9a99c
Beverly Pham <beverly.pham@gmail.com>
2018-03-27 19:53:19 -0700

    refactor test

--- challenges/largest-product/test_largest_product.py
+++ challenges/largest-product/test_largest_product.py
@@ -2,6 +2,15 @@ from largest_product import largest_product
 
 
 def test_largest_product():
+    """test for the largest product"""
     assert largest_product([[1, 2], [3, 4], [5, 6], [7, 8]]) == 56
+
+
+def test_largest_product():
+    """test for the largest product"""
     assert largest_product([[8, 20], [5, 4], [100, 100], [8, 2], [9, 3], [2, 20]]) == 10000
+
+
+def test_largest_product():
+    """test for the largest product"""
     assert largest_product([[3, 8], [4, 10], [10, 50]]) == 500
