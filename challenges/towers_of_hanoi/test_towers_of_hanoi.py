081593e57e0b11c68275d333ccf1e2831d1f4fa2
Beverly Pham <beverly.pham@gmail.com>
2018-04-05 19:48:20 -0700

    test passing but not working correctly

--- challenges/towers_of_hanoi/test_towers_of_hanoi.py
+++ challenges/towers_of_hanoi/test_towers_of_hanoi.py
@@ -1,4 +1,17 @@
-from towers_of_hanoi import towers_of_hanoi
+from .towers_of_hanoi import towers_of_hanoi
 
-def test_towers_of_hanoi():
-    assert
+
+def test_towers_of_hanoi_two():
+    assert towers_of_hanoi(2) == (3)
+
+
+def test_towers_of_hanoi_three():
+    assert towers_of_hanoi(3) == (7)
+
+
+def test_towers_of_hanoi_seven():
+    assert towers_of_hanoi(7) == (127)
+
+
+def test_towers_of_hanoi_ten():
+    assert towers_of_hanoi(10) == (1023)
--- challenges/towers_of_hanoi/towers_of_hanoi.py
+++ challenges/towers_of_hanoi/towers_of_hanoi.py
@@ -1,48 +1,74 @@
-class Node:
-    """Create a Node object"""
+# class Node:
+#     """Create a Node object"""
 
-    def __init__(self, val, next=None):
-        """Constructor for the Node object"""
-        self.val = val
-        self._next = next
-        if val is None:
-            raise TypeError('Must pass a value')
+#     def __init__(self, val, next=None):
+#         """Constructor for the Node object"""
+#         self.val = val
+#         self._next = next
+#         if val is None:
+#             raise TypeError('Must pass a value')
 
-    def __repr__(self):
-        return '{val}'.format(val=self.val)
+#     def __repr__(self):
+#         return '{val}'.format(val=self.val)
 
 
-class Stack:
-    """Create a Stack data structure"""
-    def __init__(self, iterable=[]):
-        self.top = None
-        self._size = 0
-        if type(iterable) is not list:
-            raise TypeError('Invalid iterable')
-        for item in iterable:
-            self.push(item)
+# class Stack:
+#     """Create a Stack data structure"""
+#     def __init__(self, iterable=[]):
+#         self.top = None
+#         self._size = 0
+#         if type(iterable) is not list:
+#             raise TypeError('Invalid iterable')
+#         for item in iterable:
+#             self.push(item)
 
-    def __repr__(self):
-        return f'Top of stack is {self.top.val}'
+#     def __repr__(self):
+#         return f'Top of stack is {self.top.val}'
 
-    def push(self, val):
-        """Insert a node to top of stack"""
-        try:
-            node = Node(val, self.top)
-        except TypeError:
-            return self.top
-        self.top = node
-        self._size += 1
-        return
+#     def push(self, val):
+#         """Insert a node to top of stack"""
+#         try:
+#             node = Node(val, self.top)
+#         except TypeError:
+#             return self.top
+#         self.top = node
+#         self._size += 1
+#         return
 
-    def pop(self):
-        """Remove the top node from stack"""
-        removed_node = self.top
-        self.top = self.top._next
-        self._size -= 1
-        return removed_node
+#     def pop(self):
+#         """Remove the top node from stack"""
+#         removed_node = self.top
+#         self.top = self.top._next
+#         self._size -= 1
+#         return removed_node
 
 
 def towers_of_hanoi(n):
+    a = b = c = []
+    count = 0
+    for i in reversed(range(1, n + 1)):
+        a.append(i)
+    for i in range(1, 2**n):
+        count += 1
+        if i % 3 == 1:
+            c.append(a.pop())
+        if i % 3 == 2:
+            b.append(a.pop())
+        if i % 3 == 0:
+            b.append(c.pop())
+    if n % 2 == 0:
+        return count
+    if n % 2 == 1:
+        return count
 
-    return
+
+
+# def moveTower(height,fromPole, toPole, withPole):
+#     if height >= 1:
+#         moveTower(height-1,fromPole,withPole,toPole)
+#         moveDisk(fromPole,toPole)
+#         moveTower(height-1,withPole,toPole,fromPole)
+
+# def moveDisk(fp,tp):
+#     print("moving disk from",fp,"to",tp)
+
