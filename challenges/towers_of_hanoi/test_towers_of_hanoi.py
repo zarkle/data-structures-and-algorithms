98ff086b36de286653590c0477701d8b082a032f
Beverly Pham <beverly.pham@gmail.com>
2018-04-05 21:05:17 -0700

    got it

--- challenges/towers_of_hanoi/test_towers_of_hanoi.py
+++ challenges/towers_of_hanoi/test_towers_of_hanoi.py
@@ -2,16 +2,16 @@ from .towers_of_hanoi import towers_of_hanoi
 
 
 def test_towers_of_hanoi_two():
-    assert towers_of_hanoi(2) == (3, [1, 2])
+    assert towers_of_hanoi(2) == (3, [2, 1])
 
 
 def test_towers_of_hanoi_three():
-    assert towers_of_hanoi(3) == (7, [1, 2, 3])
+    assert towers_of_hanoi(3) == (7, [3, 2, 1,])
 
 
 def test_towers_of_hanoi_seven():
-    assert towers_of_hanoi(7) == (127, [1, 2, 3, 4, 5, 6, 7])
+    assert towers_of_hanoi(7) == (127, [7, 6, 5, 4, 3, 2, 1])
 
 
 def test_towers_of_hanoi_ten():
-    assert towers_of_hanoi(10) == (1023, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    assert towers_of_hanoi(10) == (1023, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
--- challenges/towers_of_hanoi/towers_of_hanoi.py
+++ challenges/towers_of_hanoi/towers_of_hanoi.py
@@ -1,65 +1,20 @@
-# class Node:
-#     """Create a Node object"""
-
-#     def __init__(self, val, next=None):
-#         """Constructor for the Node object"""
-#         self.val = val
-#         self._next = next
-#         if val is None:
-#             raise TypeError('Must pass a value')
-
-#     def __repr__(self):
-#         return '{val}'.format(val=self.val)
-
-
-# class Stack:
-#     """Create a Stack data structure"""
-#     def __init__(self, iterable=[]):
-#         self.top = None
-#         self._size = 0
-#         if type(iterable) is not list:
-#             raise TypeError('Invalid iterable')
-#         for item in iterable:
-#             self.push(item)
-
-#     def __repr__(self):
-#         return f'Top of stack is {self.top.val}'
-
-#     def push(self, val):
-#         """Insert a node to top of stack"""
-#         try:
-#             node = Node(val, self.top)
-#         except TypeError:
-#             return self.top
-#         self.top = node
-#         self._size += 1
-#         return
-
-#     def pop(self):
-#         """Remove the top node from stack"""
-#         removed_node = self.top
-#         self.top = self.top._next
-#         self._size -= 1
-#         return removed_node
-
-
-def towers_of_hanoi(n):
-    a = b = c = []
-    count = 0
-    for i in reversed(range(1, n + 1)):
-        a.append(i)
-    for i in range(1, 2**n):
-        count += 1
-        if i % 3 == 1:
-            c.append(a.pop())
-        if i % 3 == 2:
-            b.append(a.pop())
-        if i % 3 == 0:
-            b.append(c.pop())
-    if n % 2 == 0:
-        return (count, b)
-    if n % 2 == 1:
-        return (count, c)
+# def towers_of_hanoi(n):
+#     a = b = c = []
+#     count = 0
+#     for i in reversed(range(1, n + 1)):
+#         a.append(i)
+#     for i in range(1, 2**n):
+#         count += 1
+#         if i % 3 == 1:
+#             c.append(a.pop())
+#         if i % 3 == 2:
+#             b.append(a.pop())
+#         if i % 3 == 0:
+#             b.append(c.pop())
+#     if n % 2 == 0:
+#         return (count, b)
+#     if n % 2 == 1:
+#         return (count, c)
 
 
 
@@ -72,7 +27,51 @@ def towers_of_hanoi(n):
 # def moveDisk(fp,tp):
 #     print("moving disk from",fp,"to",tp)
 
-https://www.geeksforgeeks.org/c-program-for-tower-of-hanoi/
-https://www.geeksforgeeks.org/iterative-tower-of-hanoi/
-http://interactivepython.org/runestone/static/pythonds/Recursion/TowerofHanoi.html
-https://www.cs.cmu.edu/~cburch/survey/recurse/hanoiimpl.html
+
+# def towers_of_hanoi(n):
+#     a = []
+#     b = []
+#     c = []
+#     for i in reversed(range(1, n + 1)):
+#         a.append(i)
+#     count = recur(n, a, b, c)
+#     return (count, c)
+
+
+# def recur(n, a, b, c):
+#     count = 0
+#     if n >= 1:
+#         first = recur(n - 1, a, c, b)
+#         print('moving disk {} from a to c'.format(a[-1]))
+#         c.append(a.pop())
+
+#         count += 1
+#         second = recur(n - 1, c, b, a)
+#         count += first + second
+#     return count
+# https://www.geeksforgeeks.org/c-program-for-tower-of-hanoi/
+# https://www.geeksforgeeks.org/iterative-tower-of-hanoi/
+# http://interactivepython.org/runestone/static/pythonds/Recursion/TowerofHanoi.html
+# https://www.cs.cmu.edu/~cburch/survey/recurse/hanoiimpl.html
+
+
+
+def towers_of_hanoi(n):
+    a = []
+    b = []
+    c = []
+    for i in reversed(range(1, n + 1)):
+        a.append(i)
+    count = recur(n, a, c, b)
+    return (count, c)
+
+
+def recur(n, a, c, b):
+    count = 0
+    if n >= 1:
+        first = recur(n - 1, a, b, c)
+        c.append(a.pop())
+        count += 1
+        second = recur(n - 1, b, c, a)
+        count += first + second
+    return count
