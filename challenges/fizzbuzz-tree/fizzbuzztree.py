fe5dfa21c15f6ae6831c62cca2ad2d4b82e6e127
Beverly Pham <beverly.pham@gmail.com>
2018-04-09 13:51:21 -0700

    fizzbuzz done and tested

--- challenges/fizzbuzz-tree/fizzbuzztree.py
+++ challenges/fizzbuzz-tree/fizzbuzztree.py
@@ -1,3 +1,88 @@
-def
+class Node:
+    """create a Node"""
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
 
+    def __repr__(self):  # pragma: no cover
+        return 'Node Val: {}'.format(self.val)
+
+    def __str__(self):  # pragma: no cover
+        return self.val
+
+
+class BST:
+    """create a binary search tree data structure"""
+    def __init__(self, iterable=[]):
+        self.root = None
+        if type(iterable) is not list:
+            raise TypeError
+        for item in iterable:
+            self.insert(item)
+
+    def __repr__(self):  # pragma: no cover
+        return '<BST Root {}>'.format(self.root.val)
+
+    def __str__(self):  # pragma: no cover
+        return self.root.val
+
+    def in_order(self, operation):
+        """insert in order"""
+        def _walk(node=None):
+            if node is None:
+                return
+
+            if node.left is not None:
+                _walk(node.left)
+
+            operation(node)
+
+            if node.right is not None:
+                _walk(node.right)
+
+        _walk(self.root)
+
+    def insert(self, val):
+        """insert node into BST"""
+        node = Node(val)
+        current = self.root
+
+        if self.root is None:
+            self.root = node
+            return node
+
+        while current:
+            if val >= current.val:
+                if current.right is not None:
+                    current = current.right
+                else:
+                    current.right = node
+                    break
+            elif val < current.val:
+                if current.left is not None:
+                    current = current.left
+                else:
+                    current.left = node
+                    break
+
+        return node
+
+
+def fizzbuzztree(tree):
+    """Conduct “FizzBuzz” on a tree while traversing through it. Uses in_order traversal"""
+    tree.in_order(replace)
+    return tree
+
+
+def replace(node):
+    """fizz buzz helper function"""
+    if type(node.val) is int:
+        if node.val % 15 == 0:
+            node.val = 'fizz buzz'
+        elif node.val % 3 == 0:
+            node.val = 'fizz'
+        elif node.val % 5 == 0:
+            node.val = 'buzz'
     return
+
--- challenges/fizzbuzz-tree/test_fizzbuzztree.py
+++ challenges/fizzbuzz-tree/test_fizzbuzztree.py
@@ -1,4 +1,29 @@
-import name
+from fizzbuzztree import BST, fizzbuzztree
 
-def test_name():
-    assert
+
+def test_fizzbuzztree_balanced():
+    """test fizz buzz on balanced tree"""
+    balanced_bst = BST([10, 7, 3, 16, 15, 8, 20])
+    fizzbuzztree(balanced_bst)
+    assert balanced_bst.root.val == 'buzz'
+    assert balanced_bst.root.right.left.val == 'fizz buzz'
+    assert balanced_bst.root.left.left.val == 'fizz'
+
+
+def test_fizzbuzztree_right():
+    """test fizz buzz on right-heavy tree"""
+    right_bst = BST([1, 3, 5, 7, 9, 15, 20, 21, 26])
+    fizzbuzztree(right_bst)
+    assert right_bst.root.right.val == 'fizz'
+    assert right_bst.root.right.right.val == 'buzz'
+    assert right_bst.root.right.right.right.right.right.val == 'fizz buzz'
+    assert right_bst.root.right.right.right.right.right.right.right.val == 'fizz'
+
+
+def test_fizzbuzztree_left():
+    """test fizz buzz on left-heavy tree"""
+    left_bst = BST([30, 10, 8, 6, 3])
+    fizzbuzztree(left_bst)
+    assert left_bst.root.val == 'fizz buzz'
+    assert left_bst.root.left.val == 'buzz'
+    assert left_bst.root.left.left.left.left.val == 'fizz'
