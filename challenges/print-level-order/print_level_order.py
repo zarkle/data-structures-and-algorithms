afa24fa05468c8e74b9c6633497e64161f8f2362
Beverly Pham <beverly.pham@gmail.com>
2018-04-24 16:11:13 -0700

    function written and tested, 100% coverage

--- challenges/print-level-order/print_level_order.py
+++ challenges/print-level-order/print_level_order.py
@@ -1,3 +1,26 @@
-def print_level_order(tree):
+from queue import Queue
 
-    return
+
+def print_level_order(tree):
+    """
+    print level order of K-ary tree
+    - input: k-ary tree
+    - output: return a string that contains a listing of all values in the tree, with new lines in-between each level of the tree
+    """
+    queue = Queue()
+    next_queue = Queue()
+    final = ''
+
+    queue.enqueue(tree.root)
+
+    while queue or next_queue:
+        if not queue:
+            queue, next_queue = next_queue, queue
+            final += '\n'
+
+        current = queue.dequeue()
+        final += str(current.val) + ' '
+        for child in current.children:
+            next_queue.enqueue(child)
+
+    return final
--- challenges/print-level-order/test_print_level_order.py
+++ challenges/print-level-order/test_print_level_order.py
@@ -1,5 +1,50 @@
-from .print_level_order import print_level_order
+from k_tree import KTree as KT
+from print_level_order import print_level_order
+import pytest
 
 
-def test_print_level_order():
-    assert
+@pytest.fixture
+def tree():
+    tree = KT()
+    tree.insert(1, None)
+    tree.insert(2, 1)
+    tree.insert(3, 1)
+    tree.insert(4, 1)
+    tree.insert(5, 1)
+    return tree
+
+
+@pytest.fixture
+def small_tree():
+    tree = KT()
+    tree.insert(1, None)
+    tree.insert(2, 1)
+    tree.insert(3, 1)
+    return tree
+
+
+@pytest.fixture
+def large_tree():
+    tree = KT()
+    tree.insert(10, None)
+    tree.insert(2, 10)
+    tree.insert(9, 10)
+    tree.insert(3, 2)
+    tree.insert(12, 9)
+    tree.insert(13, 9)
+    return tree
+
+
+def test_print_level_order(tree):
+    """test print_level_order"""
+    assert print_level_order(tree) == '1 \n2 3 4 5 '
+
+
+def test_print_level_order_small(small_tree):
+    """test print_level_order with small tree"""
+    assert print_level_order(small_tree) == '1 \n2 3 '
+
+
+def test_print_level_order_large(large_tree):
+    """test print_level_order with large tree"""
+    assert print_level_order(large_tree) == '10 \n2 9 \n3 12 13 '
