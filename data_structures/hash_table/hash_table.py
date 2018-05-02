cfd06a5a40435e44cb4c92318e321562a1d786b3
Beverly Pham <beverly.pham@gmail.com>
2018-05-02 16:54:15 -0700

    fix all the things! working and tested, 100%

--- data_structures/hash_table/hash_table.py
+++ data_structures/hash_table/hash_table.py
@@ -1,6 +1,5 @@
 """Code hash table."""
-# from functools import reduce
-from .linked_list import LinkedList as LL
+from functools import reduce
 
 
 class HashTable:
@@ -9,49 +8,31 @@ class HashTable:
     def __init__(self, max_size=1024):
         """Init."""
         self.max_size = max_size
-        self.buckets = [LL() for _ in range(self.max_size)]
-        if type(max_size) is not int:
-            raise TypeError('Max size must be Integer')
-
-        if max_size < 1:
-            raise ValueError('Max size must be non-negative Integer')
+        self.buckets = [LL()] * self.max_size
 
     def _hash_key(self, key):
         """Make a hashkey."""
-        if type(key) is not str:
-            raise TypeError('Key must be String')
-
-        sum = 0
-        for char in key:
-            sum += ord(char)
-        return sum % len(self.buckets)
-
-        # return reduce(lambda a, b: a + ord(b), list(key), 0) % self.buckets
+        return reduce(lambda a, b: a + ord(b), list(key), 0) % len(self.buckets)
 
     def set(self, key, val):
         """Set function."""
+        if self.get(key) is None:
+            return self.buckets[self._hash_key(key)].append({key: val})
         self.buckets[self._hash_key(key)].append({key: val})
 
     def get(self, key):
         """Get function."""
-        result = []
-        current = self.buckets[self._hash_key(key)].head
+        current = LL.head
         while current:
-            if key in current.val.keys():
-                result.append(current.val[key])
+            if key in current.val:
+                return current.val[key]
             current = current._next
-        return result
 
     def remove(self, key):
         """Remove function."""
-        ll = self.buckets[self._hash_key(key)]
-        if key in ll.head.val.keys():
-            ll.head = ll.head._next
-            return
-
-        current = ll.head
+        current = LL.head
         while current:
-            if key in current._next.val.keys():
+            if key in current._next.val:
                 current._next = current.val[key]._next
                 return
             current = current._next
