a759874041e5e5c161b8afdf7b6ff0d5b23f0341
Beverly Pham <beverly.pham@gmail.com>
2018-05-01 22:40:11 -0700

    refactored to not use intersection method

--- challenges/tree_intersection/test_tree_intersection.py
+++ challenges/tree_intersection/test_tree_intersection.py
@@ -1,3 +1,4 @@
+import pytest
 from .bst import BST
 from .tree_intersection import tree_intersection
 
@@ -12,6 +13,17 @@ def test_tree_intersection_two():
     assert tree_intersection(BST(['f', 'r', 'a', 'u', 'd']), BST(['p', 'a', 't'])) == {'a'}
 
 
-def test_tree_intersection_three():
+def test_tree_intersection_third():
     """Test tree intersection."""
     assert tree_intersection(BST([10, 7, 3, 16, 12, 8, 20]), BST([10, 12, 14, 16])) == {10, 12, 16}
+
+
+def test_tree_intersection_fourth():
+    """Test tree intersection."""
+    assert tree_intersection(BST(['start', 'stop']), BST(['stop', 'copying'])) == {'stop'}
+
+
+def test_tree_intersection_error():
+    """Test tree intersection error without binary tree."""
+    with pytest.raises(AttributeError):
+        assert tree_intersection(1, 2)
--- challenges/tree_intersection/tree_intersection.py
+++ challenges/tree_intersection/tree_intersection.py
@@ -1,12 +1,15 @@
-from .bst import BST
+"""Find common values in 2 binary trees."""
 
 
 def tree_intersection(bst1, bst2):
-    """Return a set of values found in two binary tree parameters."""
-    set1 = set()
-    set2 = set()
+    """Return a set of common values found in two binary tree parameters."""
+    set1, set2, perpendicular = set(), set(), set()
 
     bst1.pre_order(lambda n: set1.add(n.val))
     bst2.pre_order(lambda n: set2.add(n.val))
 
-    return set1.intersection(set2)
+    for each in set1:
+        if each in set2:
+            perpendicular.add(each)
+
+    return perpendicular
