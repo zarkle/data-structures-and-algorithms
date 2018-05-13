1ecbe9ed9ae2901024d9d79111ae602cfa44bd4c
Beverly Pham <beverly.pham@gmail.com>
2018-05-13 13:05:43 -0700

    Fix docstrings

--- data_structures/binary_search_tree/breadth_first_traversal.py
+++ data_structures/binary_search_tree/breadth_first_traversal.py
@@ -1,63 +1,9 @@
+"""Traverse a BST using a Breadth-first approach."""
 from ..queue.queue import Queue
 
 
-class Node:  # pragma: no cover
-    """create a Node"""
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-
-    def __repr__(self):
-        return 'Node Val: {}'.format(self.val)
-
-    def __str__(self):
-        return str(self.val)
-
-
-class BST:  # pragma: no cover
-    """create a binary search tree data structure"""
-    def __init__(self, iterable=[]):
-        self.root = None
-        if type(iterable) is not list:
-            raise TypeError
-        for item in iterable:
-            self.insert(item)
-
-    def __repr__(self):
-        return '<BST Root {}>'.format(self.root.val)
-
-    def __str__(self):
-        return self.root.val
-
-    def insert(self, val):
-        """insert node into BST"""
-        node = Node(val)
-        current = self.root
-
-        if self.root is None:
-            self.root = node
-            return node
-
-        while current:
-            if val >= current.val:
-                if current.right is not None:
-                    current = current.right
-                else:
-                    current.right = node
-                    break
-            elif val < current.val:
-                if current.left is not None:
-                    current = current.left
-                else:
-                    current.left = node
-                    break
-
-        return node
-
-
 def breadth_first_traversal(tree):
-    """return breadth-first-traversal of a binary tree"""
+    """Return breadth-first-traversal of a binary tree."""
     queue = Queue()
     traverse = []
 
--- data_structures/binary_search_tree/test_breadth_first_traversal.py
+++ data_structures/binary_search_tree/test_breadth_first_traversal.py
@@ -3,16 +3,19 @@ from .bst import BST
 
 
 def test_breadth_first_traversal():
+    """Test breadth-first traversal."""
     tree = BST([2, 1, 3])
     assert breadth_first_traversal(tree) == [2, 1, 3]
 
 
 def test_breadth_first_traversal_balanced():
+    """Test breadth-first traversal."""
     tree = BST([10, 7, 3, 16, 12, 8, 20])
     assert breadth_first_traversal(tree) == [10, 7, 16, 3, 8, 12, 20]
 
 
 def test_breadth_first_traversal_right():
+    """Test breadth-first traversal."""
     tree = BST([1, 3, 5, 7, 9])
     assert breadth_first_traversal(tree) == [1, 3, 5, 7, 9]
 
