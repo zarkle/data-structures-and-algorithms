7511ab390aeea8d1f34fed4d7d466acde86503b4
Beverly Pham <beverly.pham@gmail.com>
2018-05-13 13:14:07 -0700

    refactor tests to use conftest fixtures

--- data_structures/binary_search_tree/test_breadth_first_traversal.py
+++ data_structures/binary_search_tree/test_breadth_first_traversal.py
@@ -8,14 +8,12 @@ def test_breadth_first_traversal():
     assert breadth_first_traversal(tree) == [2, 1, 3]
 
 
-def test_breadth_first_traversal_balanced():
-    """Test breadth-first traversal."""
-    tree = BST([10, 7, 3, 16, 12, 8, 20])
-    assert breadth_first_traversal(tree) == [10, 7, 16, 3, 8, 12, 20]
+def test_breadth_first_traversal_balanced(balanced_bst):
+    """Test breadth-first traversal in balanced tree."""
+    assert breadth_first_traversal(balanced_bst) == [10, 7, 16, 3, 8, 12, 20]
 
 
-def test_breadth_first_traversal_right():
-    """Test breadth-first traversal."""
-    tree = BST([1, 3, 5, 7, 9])
-    assert breadth_first_traversal(tree) == [1, 3, 5, 7, 9]
+def test_breadth_first_traversal_right(right_heavy):
+    """Test breadth-first traversal in right heavy tree."""
+    assert breadth_first_traversal(right_heavy) == [1, 3, 5, 7, 9]
 
--- data_structures/binary_search_tree/test_find_maximum_value_binary_tree.py
+++ data_structures/binary_search_tree/test_find_maximum_value_binary_tree.py
@@ -2,19 +2,16 @@ from .find_maximum_value_binary_tree import find_maximum_value
 from .bst import BST
 
 
-def test_find_maximum_value_balanced():
+def test_find_maximum_value_balanced(balanced_bst):
     """Test find max value in balanced tree."""
-    balanced = BST([10, 7, 3, 16, 12, 8, 20])
-    assert find_maximum_value(balanced) == 20
+    assert find_maximum_value(balanced_bst) == 20
 
 
-def test_find_maximum_value_left():
+def test_find_maximum_value_left(left_heavy):
     """Test find max value in left-heavy tree."""
-    left = BST([10, 8, 6, 4])
-    assert find_maximum_value(left) == 10
+    assert find_maximum_value(left_heavy) == 10
 
 
-def test_find_maximum_value_right():
+def test_find_maximum_value_right(right_heavy):
     """Test find max value in right-heavy tree."""
-    right = BST([1, 3, 5, 7, 9])
-    assert find_maximum_value(right) == 9
+    assert find_maximum_value(right_heavy) == 9
