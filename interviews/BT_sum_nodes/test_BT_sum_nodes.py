7f430475907ac85bef5ade2f9c4b75b93c2f7400
Beverly Pham <beverly.pham@gmail.com>
2018-05-21 19:58:39 -0700

    can sum subtree when pass in a start value

--- interviews/BT_sum_nodes/test_BT_sum_nodes.py
+++ interviews/BT_sum_nodes/test_BT_sum_nodes.py
@@ -10,3 +10,18 @@ def test_sum_nodes():
 def test_sum_nodes_value():
     tree = BST([1, 2, 3, 4, 5])
     assert sum_nodes(tree, 3) == 12
+
+
+def test_sum_nodes_value_six():
+    tree = BST([8, 3, 1, 6, 4, 7, 10, 14, 13])
+    assert sum_nodes(tree, 6) == 17
+
+
+def test_sum_nodes_value_fourteen():
+    tree = BST([8, 3, 1, 6, 4, 7, 10, 14, 13])
+    assert sum_nodes(tree, 14) == 27
+
+
+def test_sum_nodes_value_three():
+    tree = BST([8, 3, 1, 6, 4, 7, 10, 14, 13])
+    assert sum_nodes(tree, 3) == 21
